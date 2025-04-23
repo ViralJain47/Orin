51e69cd6400f7869e9c1d79da3e1f7904b603419
ViralJain47 <viraljain4477@gmail.com>
2025-04-24 01:09:53 +0530

    posts and like functionality completed

---
+++ constants/mock-data.ts
@@ -0,0 +1,51 @@
+export const STORIES = [
+    {
+      id: "1",
+      username: "Your story",
+      avatar: "https://images.unsplash.com/photo-1527980965255-d3b416303d12?crop=faces&fit=crop&w=200&h=200",
+      hasStory: false,
+    },
+    {
+      id: "2",
+      username: "Ishita",
+      avatar: "https://images.unsplash.com/photo-1552058544-f2b08422138a?crop=faces&fit=crop&w=200&h=200",
+      hasStory: true,
+    },
+    {
+      id: "3",
+      username: "Reyansh",
+      avatar: "https://images.unsplash.com/photo-1614282842437-d6f4d7bc7b1a?crop=faces&fit=crop&w=200&h=200",
+      hasStory: true,
+    },
+    {
+      id: "4",
+      username: "Tanya",
+      avatar: "https://images.unsplash.com/photo-1544005313-94ddf0286df2?crop=faces&fit=crop&w=200&h=200",
+      hasStory: true,
+    },
+    {
+      id: "5",
+      username: "Kunal",
+      avatar: "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?crop=faces&fit=crop&w=200&h=200",
+      hasStory: true,
+    },
+    {
+      id: "6",
+      username: "Meera",
+      avatar: "https://images.unsplash.com/photo-1544723795-3fb6469f5b39?crop=faces&fit=crop&w=200&h=200",
+      hasStory: true,
+    },
+    {
+      id: "7",
+      username: "Aarav",
+      avatar: "https://images.unsplash.com/photo-1623690931802-02c3e0d1c442?crop=faces&fit=crop&w=200&h=200",
+      hasStory: true,
+    },
+    {
+      id: "8",
+      username: "Neha",
+      avatar: "https://images.unsplash.com/photo-1524504388940-b1c1722653e1?crop=faces&fit=crop&w=200&h=200",
+      hasStory: true,
+    }
+  ];
+  
